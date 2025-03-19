3be79e1d250b19540018509d729c777c0c672a02
Ethan Wilson <ehwilson.business@gmail.com>
2025-03-19 18:31:49 -0400

    created 3 more sets of list items for the radio buttons. gave each radio groups their own v-model and name to prevent selecting differnt group options. added indexing to select a default radio button selection

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
     <Beverage :isIced="currentTemp === 'Cold'" />
     <ul>
       <li>
-        <template v-for="temp in temps" :key="temp">
+        <template v-for="(temp, index) in temps" :key="temp">
           <label>
             <input
               type="radio"
               name="temperature"
               :id="`r${temp}`"
               :value="temp"
+              :checked="index === 0"
               v-model="currentTemp"
             />
             {{ temp }}
@@ -17,12 +18,73 @@
         </template>
       </li>
     </ul>
+
+    <ul>
+      <li>
+        <template v-for="(base, index) in bases" :key="base">
+          <label>
+            <input
+              type="radio"
+              name="base"
+              :id="`r${base.id}`"
+              :value="base.id"
+              :checked="index === 0"
+              v-model="baseOption"
+            />
+            {{ base.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <template v-for="(cream, index) in creamers" :key="cream">
+          <label>
+            <input
+              type="radio"
+              name="creamer"
+              :id="`r${cream.id}`"
+              :value="cream.id"
+              :checked="index === 0"
+              v-model="creamerOption"
+            />
+            {{ cream.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <template v-for="(syrup, index) in syrups" :key="syrup">
+          <label>
+            <input
+              type="radio"
+              name="syrup"
+              :id="`r${syrup.id}`"
+              :value="syrup.id"
+              :checked="index===0"
+              v-model="syrupOption"
+            />
+            {{ syrup.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
   </div>
+
+
+
+
+
+
 </template>
 
 <script setup lang="ts">
 import Beverage from "./components/Beverage.vue";
-import { temps, currentTemp } from "./stores/beverage";
+import { temps, currentTemp, bases, syrups, creamers } from "./stores/beverage";
+
 </script>
 
 <style lang="scss">
