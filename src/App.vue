edc76f4c94541e21e5fd0a7033e55913b301be19
Ethan Wilson <ehwilson.business@gmail.com>
2025-03-19 21:52:05 -0400

    moved changed from stackblitz to here. created some new const variables to reflect each target. created functions to update the values of the components. initialized @change tags with the functions to update colors on click. renamed imports component names

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,13 @@
     <Beverage :isIced="currentTemp === 'Cold'" />
     <ul>
       <li>
-        <template v-for="(temp, index) in temps" :key="temp">
+        <template v-for="temp in temps" :key="temp">
           <label>
             <input
               type="radio"
               name="temperature"
               :id="`r${temp}`"
               :value="temp"
-              :checked="index === 0"
               v-model="currentTemp"
             />
             {{ temp }}
@@ -19,20 +18,23 @@
       </li>
     </ul>
 
+    <BaseBeverage :color="color"/>
     <ul>
       <li>
         <template v-for="(base, index) in bases" :key="base">
           <label>
             <input
+              @change="updateBaseColor"
               type="radio"
               name="base"
               :id="`r${base.id}`"
-              :value="base.id"
+              :value="base.color"
               :checked="index === 0"
-              v-model="baseOption"
+              v-model="baseColor"
             />
             {{ base.name }}
           </label>
+          <baseBeverage :class="baseColor"/>
         </template>
       </li>
     </ul>
@@ -42,15 +44,17 @@
         <template v-for="(cream, index) in creamers" :key="cream">
           <label>
             <input
+              @change="updateCreamColor"
               type="radio"
               name="creamer"
               :id="`r${cream.id}`"
-              :value="cream.id"
+              :value="cream.color"
               :checked="index === 0"
-              v-model="creamerOption"
+              v-model="creamerColor"
             />
             {{ cream.name }}
           </label>
+          <froth :class="creamColor"/>
         </template>
       </li>
     </ul>
@@ -60,30 +64,46 @@
         <template v-for="(syrup, index) in syrups" :key="syrup">
           <label>
             <input
+              @change="updateSyrupColor"
               type="radio"
               name="syrup"
               :id="`r${syrup.id}`"
-              :value="syrup.id"
+              :value="syrup.color"
               :checked="index===0"
-              v-model="syrupOption"
+              v-model="syrupColor"
             />
             {{ syrup.name }}
           </label>
+          <froth :class="syrupColor"/>
         </template>
       </li>
     </ul>
   </div>
-
-
-
-
-
-
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Beverage from "./components/Beverage.vue";
-import { temps, currentTemp, bases, syrups, creamers } from "./stores/beverage";
+import { temps, currentTemp } from "./stores/beverage";
+import { bases, creamers, syrups } from "./stores/beverage";
+import Base from "./components/Base.vue"
+import Creamer from "./components/Creamer.vue"
+import Syrup from "./components/Syrup.vue"
+
+const baseColor = ref(bases[0]?.color || null);
+const updateBaseColor = (event) => {
+  baseColor.value = event.target.value
+}
+
+const creamColor = ref(null)
+const updateCreamColor = (event) => {
+  creamColor.value = event.target.value
+}
+
+const syrupColor = ref(null)
+const updateSyrupColor = (event) => {
+  syrupColor.value = event.target.value
+}
 
 </script>
 
