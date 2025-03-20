c810b65c22531113ba6d7fd0fe86bea1948950e8
Ethan Wilson <ehwilson.business@gmail.com>
2025-03-19 23:15:36 -0400

    created new variables to be exported so they could be used as directives. changesd the default value of the current temp to hot

--- src/App.vue
+++ src/App.vue
@@ -18,92 +18,72 @@
       </li>
     </ul>
 
-    <BaseBeverage :color="color"/>
+    <BaseBeverage :color="currentBase.color" />
     <ul>
       <li>
-        <template v-for="(base, index) in bases" :key="base">
+        <template v-for="(base, index) in bases" :key="base.id">
           <label>
             <input
-              @change="updateBaseColor"
               type="radio"
               name="base"
               :id="`r${base.id}`"
-              :value="base.color"
+              :value="base"
               :checked="index === 0"
-              v-model="baseColor"
+              v-model="currentBase"
             />
             {{ base.name }}
           </label>
-          <baseBeverage :class="baseColor"/>
         </template>
       </li>
     </ul>
 
     <ul>
       <li>
-        <template v-for="(cream, index) in creamers" :key="cream">
+        <template v-for="(cream, index) in creamers" :key="cream.id">
           <label>
             <input
-              @change="updateCreamColor"
               type="radio"
               name="creamer"
               :id="`r${cream.id}`"
-              :value="cream.color"
+              :value="cream"
               :checked="index === 0"
-              v-model="creamerColor"
+              v-model="currentCream"
             />
             {{ cream.name }}
           </label>
-          <froth :class="creamColor"/>
         </template>
       </li>
     </ul>
 
     <ul>
       <li>
-        <template v-for="(syrup, index) in syrups" :key="syrup">
+        <template v-for="(syrup, index) in syrups" :key="syrup.id">
           <label>
             <input
-              @change="updateSyrupColor"
               type="radio"
               name="syrup"
               :id="`r${syrup.id}`"
-              :value="syrup.color"
+              :value="syrup"
               :checked="index===0"
-              v-model="syrupColor"
+              v-model="currentSyrup"
             />
             {{ syrup.name }}
           </label>
-          <froth :class="syrupColor"/>
         </template>
       </li>
     </ul>
+
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import Beverage from "./components/Beverage.vue";
-import { temps, currentTemp } from "./stores/beverage";
-import { bases, creamers, syrups } from "./stores/beverage";
+import { currentTemp, currentBase, currentCream, currentSyrup } from "./stores/beverage";
+import { temps, bases, creamers, syrups } from "./stores/beverage";
 import Base from "./components/Base.vue"
 import Creamer from "./components/Creamer.vue"
 import Syrup from "./components/Syrup.vue"
-
-const baseColor = ref(bases[0]?.color || null);
-const updateBaseColor = (event) => {
-  baseColor.value = event.target.value
-}
-
-const creamColor = ref(null)
-const updateCreamColor = (event) => {
-  creamColor.value = event.target.value
-}
-
-const syrupColor = ref(null)
-const updateSyrupColor = (event) => {
-  syrupColor.value = event.target.value
-}
 
 </script>
 
